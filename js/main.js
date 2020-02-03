493b2b84424d01ce33f0d58d0d4cad6d4af71988
imgios <sulipano.gs@gmail.com>
2020-02-03 18:30:37 +0100

    Adds the missing js file and small fixes

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// Tooltips
+$(function () {
+	$('[data-toggle="tooltip"]').tooltip()
+})
+
+// Age
+$(function () {
+	var age = new Date(new Date() - new Date(1996, 9, 19)).getFullYear() - 1970;
+	$(".age").html(age);
+})
