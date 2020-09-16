9db0db73e0f96d76fd43eba365d4bd8ae45dc1ee
Giosuè Sulipano <sulipano.gs@gmail.com>
2020-09-17 01:57:19 +0200

    Add top 5 repos based on stars

--- js/stats.js
+++ js/stats.js
@@ -7,6 +7,7 @@ async function getUserData() {
     const resJson = await res.json();
 
     fillUserData(resJson);
+    getTopRepos();
 }
 
 function fillUserData(data) {
@@ -17,4 +18,28 @@ function fillUserData(data) {
     document.getElementById("github-followers").innerText = data.followers;
     document.getElementById("github-following").innerText = data.following;
     document.getElementById("github-repos").innerText = data.public_repos;
+}
+
+async function getTopRepos() {
+    const res = await fetch(API_BASE_URL + '/repos');
+    const resJson = await res.json();
+
+    fillUserTopRepos(resJson);
+}
+
+function fillUserTopRepos(repos) {
+    repos
+    .sort((a,b) => (a.stargazers_count > b.stargazers_count) ? -1 : 1)
+    .slice(0,5)
+    .forEach(repo => {
+        const listItem = document.createElement('li');
+        const element = document.createElement('a');
+        element.classList.add('badge', 'badge-primary')
+        element.href = repo.html_url;
+        element.target = '_blank';
+        element.innerText = repo.name;
+
+        listItem.appendChild(element);
+        document.getElementById('top-repos').appendChild(listItem);
+    });
 }
