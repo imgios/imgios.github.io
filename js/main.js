44d632163bb21d8bc4ff655f0d3e7cc5340a1549
Giosuè Sulipano <sulipano.gs@gmail.com>
2021-11-15 22:15:01 +0100

    🌑 Going dark

--- js/main.js
+++ js/main.js
@@ -1,10 +1,22 @@
-// Tooltips
-$(function () {
-	$('[data-toggle="tooltip"]').tooltip();
-})
-
 // Age
 $(function () {
 	var age = new Date(new Date() - new Date(1996, 9, 19)).getFullYear() - 1970;
-	$(".age").html(age);
-})
+	$("#age").html(age);
+});
+
+// Dark Theme
+$(function () {
+	const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+
+	if (prefersDarkScheme.matches) {
+	  document.body.classList.add("bg-dark");
+		document.body.classList.add("text-light")
+		document.body.classList.remove("bg-light");
+		document.body.classList.remove("text-dark");
+	} else {
+		document.body.classList.add("bg-light");
+		document.body.classList.add("text-dark")
+	  document.body.classList.remove("bg-dark");
+		document.body.classList.remove("text-light");
+	}
+});
